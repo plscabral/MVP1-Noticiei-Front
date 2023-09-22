b620003b3165c913707ab362cb8c49ccf95fb7b0
Paulo Cabral <paulo.luiz127@gmail.com>
2023-09-22 01:54:00 -0300

    feat: implementando docker na aplicação.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, // necessário para que o mapeamento da porta do Docker Container funcione
+		strictPort: true,
+		port: 3333, // você pode substituir esta porta por qualquer porta
+	}
+});
